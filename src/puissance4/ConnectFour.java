5702f70c6792474d0997d90a5971ffb0f151df42
Raycki <mist-flow@hotmail.fr>
2014-12-12 17:44:53 +0100

    end tp1

--- src/puissance4/ConnectFour.java
+++ src/puissance4/ConnectFour.java
@@ -1,11 +1,22 @@
 package puissance4;
 
+import java.util.List;
+
 import gameboard.GameBoard;
 import gameboard.Player;
 import gameboard.Turn;
 
 
 public class ConnectFour extends GameBoard {
+
+	public ConnectFour(int width, int length, List<Turn> history) {
+		super(width, length, history);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ConnectFour(int width, int length){
+		super(width,length);
+	}
 
 	@Override
 	public void play(Turn t) {
